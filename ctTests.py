5a6f35ed7464dd55976a2bb1f5af8ce2c08968e0
Kuan-Hsun Chen <c0066c@gmail.com>
2017-04-25 11:52:04 +0200

    Done for all tests

--- ctTests.py
+++ ctTests.py
@@ -16,13 +16,16 @@ def k2uFirstCarryinUbound(k, rest):
     else:
         return False
 def k2uSecondBlockingUbound(k, rest):
-    #print (k['shared-R']+k['exclusive-R']+quicksum(min(i['shared-R'], i['exclusive-R']) for i in rest))/k['period']+quicksum(utili(i) for i in rest)
-    #print (len(rest)+1)
-    #print (2**(1/(len(rest)+1))-1)
     tmpSum = 0.0
+    tmpSumP = 0.0
     for i in rest:
         tmpSum += min(i['shared-R'], i['exclusive-R'])
-    if (k['shared-R']+k['exclusive-R']+tmpSum)<=(len(rest)+1)*(2**(1/(len(rest)+1))-1):
+        tmpSumP += utili(i)
+
+    #print (k['shared-R']+k['exclusive-R']+tmpSum)/k['period']+tmpSumP
+    #print (len(rest)+1)
+    #print (2**(1/(len(rest)+1))-1)
+    if ((k['shared-R']+k['exclusive-R']+tmpSum)/k['period'])+tmpSumP<=(len(rest)+1)*(2**(1/(len(rest)+1))-1):
         return True
     else:
         return False
