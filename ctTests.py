a277e93d6917fb1b90984734398895d89179c8f2
Kuan-Hsun Chen <c0066c@gmail.com>
2017-04-25 02:05:17 +0200

    Fix bugs in ctTests

--- ctTests.py
+++ ctTests.py
@@ -1,25 +1,46 @@
+from __future__ import division
 from gurobipy import *
 from miscs import *
 import numpy as np
 
 #following Tests are prepared for double checking
 def k2uFirstCarryinUbound(k, rest):
-    if quicksum(utili(i) for i in rest) <= np.log(3/(utiliAddE(k)+2)):
+    tmpSum = 0.0
+    for i in rest:
+        tmpSum += utili(i)
+    if tmpSum <= np.log(3/(utiliAddE(k)+2)):
+        #print quicksum(utili(i) for i in rest)
+        #print np.log(3/(utiliAddE(k)+2))
         return True
     else:
         return False
 def k2uSecondBlockingUbound(k, rest):
-    if (k['shared-R']+k['exclusive-R']+quicksum(min(i['shared-R'], i['exclusive-R']) for i in rest))/k['period']+quicksum(utili(i) for i in rest)<=(len(rest)+1)*(2**(1/(len(rest)+1))-1):
+    #print (k['shared-R']+k['exclusive-R']+quicksum(min(i['shared-R'], i['exclusive-R']) for i in rest))/k['period']+quicksum(utili(i) for i in rest)
+    #print (len(rest)+1)
+    #print (2**(1/(len(rest)+1))-1)
+    tmpSum = 0.0
+    for i in rest:
+        tmpSum += min(i['shared-R'], i['exclusive-R'])
+    if (k['shared-R']+k['exclusive-R']+tmpSum)<=(len(rest)+1)*(2**(1/(len(rest)+1))-1):
         return True
     else:
         return False
 def k2qJitterBound(k, rest):
-    if (k['shared-R']+k['exclusive-R']+quicksum(vfunc(i) for i in rest))/1-quicksum(utili(i) for i in rest) <= k['period']:
+    tmpSum = 0.0
+    tmpSumP = 0.0
+    for i in rest:
+        tmpSum += vfunc(i)
+        tmpSumP += utili(i)
+    #if (k['shared-R']+k['exclusive-R']+tmpSum)/1-tmpSumP <= k['period']:
+    if utiliAddE(k)+tmpSum/k['period']+tmpSumP<=1 and tmpSumP <1:
         return True
     else:
         return False
 def inflation(k, rest, alltasks):
-    if utili(k)+quicksum(utili(i) for i in rest) <= np.log(3/(2+max(utiliAddE(j) for j in alltasks))):
+    tmpSum = 0.0
+    for i in rest:
+        tmpSum += utili(i)
+    if utili(k)+tmpSum <= np.log(3/(2+max(utiliAddE(j) for j in alltasks))):
         return True
     else:
         return False
