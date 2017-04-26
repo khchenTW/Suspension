9df04093a1fafc90c1aa8d9686dd4629f3918c94
Kuan-Hsun Chen <c0066c@gmail.com>
2017-04-26 11:39:40 +0200

    Ready for distributing to the servers

--- partition_eval.py
+++ partition_eval.py
@@ -12,26 +12,34 @@ from scipy.stats.mstats import gmean
 def main():
 
     args = sys.argv
-    if len(args) < 3:
+    if len(args) < 4:
+        print "Usage: python partition_eval.py [# of sets] [generate] [stype] [group]"
         return -1 # no input
     tasksets_amount = int (args[1])
-    stype = args[3] # S, M, L
     mode = int(args[2]) # 0 = generate and use, 1 = directly use the inputs
-    inputfiles_amount = 1 # n for distribution
+    stype = args[3] # S, M, L
+    group = args[4] # this should be less than inputfiles_amount
+    inputfiles_amount = 4 # n for distribution
     tasksets_amount = int(math.ceil(tasksets_amount / inputfiles_amount))
+    '''
     dist_utilizations = {
         "10Tasks" : 10,
         "20Tasks" : 20,
         "30Tasks" : 30,
         #"40Tasks" : 40,
     }
+    '''
+    dist_utilizations = OrderedDict()
+    dist_utilizations['10Tasks'] = 10
+    dist_utilizations['20Tasks'] = 20
+    dist_utilizations['30Tasks'] = 30
 
-    if mode == 0:
-        idx = 0
-        perAmount = [[] for i in range(len(dist_utilizations.items()))] # since 3 items in dict
-        for set_name, amount in dist_utilizations.items():
-            for uti in range(int(100/10*amount), int(500/10*amount)+1, 200):
-                for j in range(inputfiles_amount):
+    idx = 0
+    perAmount = [[] for i in range(len(dist_utilizations.items()))] # since 3 items in dict
+    for set_name, amount in dist_utilizations.items():
+        for uti in range(int(100/10*amount), int(500/10*amount)+1, 200):
+            for j in range(inputfiles_amount):
+                if mode == 0:
                     if stype == 'S':
                         tasksets = [generator.taskGeneration(amount, uti, 'S') for n in range(tasksets_amount)]
                         np.save ('input/'+str(set_name)+'_'+str(uti)+'_'+str(j)+'_S', tasksets)
@@ -43,13 +51,15 @@ def main():
                     elif stype == 'L':
                         tasksets = [generator.taskGeneration(amount, uti, 'L') for n in range(tasksets_amount)]
                         np.save ('input/'+str(set_name)+'_'+str(uti)+'_'+str(j)+'_L', tasksets)
-                    perAmount[idx].append('input/'+str(set_name)+'_'+str(uti)+'_'+str(j)+'_'+str(stype)+'.npy')
-            idx+=1
-
+                else:
+                    #TODO check if the inputs are there.
+            perAmount[idx].append('input/'+str(set_name)+'_'+str(uti)+'_'+str(group)+'_'+str(stype)+'.npy')
+        idx+=1
+    print perAmount
     gRes=[[] for i in range(13)] # 13 methods
     for idx, filenames  in enumerate(perAmount):
         fileA = 'tasks'+repr((1+idx)*10)+'_stype'+repr(stype)
-        file = open(fileA + '.txt', "w")
+        file = open('output/'+fileA + '.txt', "w")
         file.write('[ILPcarry, ILPblock, ILPjit, inflation, TDAcarry, TDAblock, TDAjit, TDAjitblock, TDAmix, CTcarry, CTblock, CTjit, CTmix]\n')
         for filename in filenames:
             file.write(filename+'\n')
